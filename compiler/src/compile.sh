85ad5ca53ea080da69e863e68c3cbea356774955
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2025-07-18 23:20:05 +0200

    Forse remove all files in the output if recompiling everything

--- compiler/src/compile.sh
+++ compiler/src/compile.sh
@@ -81,10 +81,8 @@ TO_DELETE=()
 if [ "$COMPILE_ALL" == "--all" ] || ! git rev-parse "${BRANCH}~1" >/dev/null 2>&1; then
     echo "🚀 Compiling all .py files in $SRC_DIR..."
 
-    # since we're compiling everything, we can delete all .so files
-    find "$OUT_DIR" -type f -name "*.so" | while IFS= read -r line; do
-        TO_DELETE+=("$line")
-    done
+    # since we're compiling everything, we can delete all .so files in the build folder
+    rm -rf "$OUT_DIR/*"
 
     while IFS= read -r line; do
         TO_COMPILE+=("$line")
