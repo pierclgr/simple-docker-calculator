af51bb3ce10e0ff6313a18dabb7494dae0a938cb
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2025-07-18 22:51:58 +0200

    Changed build structure

--- src/start.sh
+++ src/start.sh
@@ -17,8 +17,5 @@ websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
 # Wait a moment for services to start
 sleep 2
 
-# Set PYTHONPATH so Python can find your modules
-export PYTHONPATH=/app/simple_docker_calculator:$PYTHONPATH
-
 # Start your Python application
 python -c "import main"
